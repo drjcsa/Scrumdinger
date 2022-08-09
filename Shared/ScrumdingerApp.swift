3aa4fb3c71317f57bc926b6b649cf158937a79b2
juan carlos <dr.jcsa@outlook.com>
2022-08-09 16:05:11 -0400

    Initial Commit

---
+++ Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by dr.jcsa on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
